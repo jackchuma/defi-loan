064b4ec75d892c323057b2bfe6562430ff3c1b78
Jack Chuma <jackchuma21@gmail.com>
2022-03-21 16:33:03 -0400

    test added: should save term of loan (in months) on deployment

--- test/Loan.test.js
+++ test/Loan.test.js
@@ -3,13 +3,14 @@ const { ethers } = require("hardhat");
 
 describe("Loan", function () {
   before(async function() {
-    const Loan = await ethers.getContractFactory("Loan");
+    this.Loan = await ethers.getContractFactory("Loan");
   });
 
   context("Deployment", async function() {
-    this.beforeEach(async function() {
-      this.loan = await Loan.deploy("");
-      await this.loan.deployed();
+    it ("Should save term of loan (in months) on deployment", async function() {
+      const loan = await this.Loan.deploy(120);
+      await loan.deployed();
+      expect((await loan.length()).toNumber()).to.equal(120);
     });
   });
 });
